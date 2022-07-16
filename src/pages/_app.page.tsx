9af4f2e0717f1ccf51eb48b41c7992c64723fc31
Harang <dbd02169@naver.com>
2022-07-16 22:52:23 +0900

    feat: 상태관리 라이브러리 세팅을 하라 (#7)
    - Server State를 관리하는 react-query 세팅
    - Client state를 관리하는 recoil 세팅
    - test wrapper component 구현

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
 import lightTheme from '@/styles/theme';
 
 function MyApp({ Component: AppComponent, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+      },
+    },
+  }));
+
   return (
-    <ThemeProvider theme={lightTheme}>
-      <AppComponent {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <ThemeProvider theme={lightTheme}>
+            <AppComponent {...pageProps} />
+          </ThemeProvider>
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
